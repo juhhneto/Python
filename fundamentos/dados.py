0c67889ae63b7c0ef5ccb4cd83099d9a2436c147
Juliana Neto <juneto.s88@gmail.com>
2026-01-05 14:04:15 -0300

    Data types

---
+++ fundamentos/dados.py
@@ -0,0 +1,10 @@
+#pythonflix
+name = "Top Gun Maverick"
+yearLaunch = 2023
+noteMovie = 9.5
+planIncluded = False
+
+print(type(name))
+print(type(yearLaunch))
+print(type(noteMovie))
+print(type(planIncluded))

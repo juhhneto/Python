5f0a47c3e3118e62e4f3bda7365a3862709488b4
Juliana Neto <juneto.s88@gmail.com>
2026-01-05 17:13:57 -0300

    add lists

---
+++ fundamentos/8-lista.py
@@ -0,0 +1,11 @@
+filmsList = ["Inception", "The Shawshank Redemption", "The Dark Knight", "Pulp Fiction", "Interstellar"]
+
+# 1 - Tamanho da lista
+print (len(filmsList))
+
+# 2 - Recuperar um item da lista pelo nome
+print(filmsList.index("Interstellar"))
+
+# 3 - Adicionar item ao final da lista
+filmsList.append("The Lord of the Rings")
+print(filmsList)

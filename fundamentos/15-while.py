2d2378456191801067838500faf486ee2ff292ea
Juliana Neto <juneto.s88@gmail.com>
2026-01-07 00:39:41 -0300

    add while

---
+++ fundamentos/15-while.py
@@ -0,0 +1,42 @@
+# Lista de Filmes
+moviesList = ["Titanic", "The Godfather", "Inception", "Jurassic Park"]
+
+# 1 = Iterando valores de uma lista de filmes usando while
+index = 0
+while index < len(moviesList):
+    print(moviesList[index])
+    index += 1
+
+# 2 - Quando a condição for atendida, o loop será encerrado
+index = 0
+while index < len(moviesList):
+    if moviesList[index] == "Inception":
+        break
+    print(moviesList[index])
+    index += 1
+
+# 3 - Quando a condição for atendida, o loop vai para próxima iteração
+index = 0
+while index <len(moviesList):
+    if moviesList[index] == "Inception":
+        continue
+    print(moviesList[index])
+    index += 1
+
+# 4 - Avaliação do filme com while
+movieName = input("Digite o nome do filme:\n")
+movieRating = int(input("Digite quantas avaliações deseja fazer:\n"))
+total = 0
+count = 0
+
+while count < movieRating:
+    note = float(input("Digite a nota para o filme:\n"))
+    total += note
+    count += 1
+
+if movieRating > 0:
+    average = total / movieRating
+else:
+    average = 0
+
+print(f"Média de avaliação do filme {movieName} é: {average:.2f}")
